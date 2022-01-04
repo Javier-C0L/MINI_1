a090c78169de287ff93f4fa47c3ba2da6c52ce4e
Javier-C0L <89871899+Javier-C0L@users.noreply.github.com>
2022-01-04 16:46:22 -0500

    Add files via upload

---
+++ GEEK_OUT_MASTERS/src/juego_GEEK_OUT_MASTERS/Carta.java
@@ -0,0 +1,4 @@
+package juego_GEEK_OUT_MASTERS;
+
+public class Carta {
+}
---
+++ GEEK_OUT_MASTERS/src/juego_GEEK_OUT_MASTERS/GeekOutMasters.java
@@ -0,0 +1,12 @@
+package juego_GEEK_OUT_MASTERS;
+
+/**
+ * GeekOutMasters apply craps rules.
+ * estado
+ * @author Javier Castrillon
+ * @author Alejandro Villamil
+ * @version V.1.0.0 date 04/01/2022
+ */
+
+public class GeekOutMasters {
+}
